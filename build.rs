2b7ec637d77c72c604bc656383fa98885f996343
AlexViaColl <alexviacoll@gmail.com>
2022-03-25 09:25:27 +0100

    Download stb_image.h at build time

--- build.rs
+++ build.rs
@@ -1,3 +1,18 @@
+use std::path::Path;
+use std::process::Command;
+
 fn main() {
     println!("cargo:rustc-link-search=.");
+
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=compile.sh");
+
+    if !Path::new("stb_image.h").exists() {
+        Command::new("/usr/bin/wget")
+            .arg("https://raw.githubusercontent.com/nothings/stb/master/stb_image.h")
+            .status()
+            .unwrap();
+    }
+
+    Command::new("/bin/sh").arg("compile.sh").status().unwrap();
 }
