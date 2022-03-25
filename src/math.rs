4e9487d9f3e6fba8ad76ec6e6de58ed05bf15409
AlexViaColl <alexviacoll@gmail.com>
2022-03-25 20:33:37 +0100

    Implement Mat4::ortho and a few other improvements to math

--- src/math.rs
+++ src/math.rs
@@ -1,11 +1,20 @@
-use std::ops::{Add, Mul, Sub};
+use std::ops::{Add, Mul, Neg, Sub};
 
+/// Linear interpolation between A and B by a "normalized percentage" T.
+/// Panics if T is not in the range from 0 to 1.
 pub fn lerp<T>(a: T, b: T, t: f32) -> T
 where
     T: Mul<f32, Output = T> + Add<Output = T>,
 {
     assert!((0.0..=1.0).contains(&t));
     a * (1.0 - t) + b * t
+}
+
+//pub fn inv_lerp<T>(a: T, b: T, value: T) -> f32
+//where
+//    T: Sub<Output = T>,
+pub fn inv_lerp(a: f32, b: f32, value: f32) -> f32 {
+    (value - a) / (b - a)
 }
 
 #[repr(C)]
@@ -31,13 +40,62 @@ pub struct Vec4 {
 pub struct Mat4([f32; 16]);
 
 impl Vec3 {
-    #[rustfmt::skip]
     pub fn new(x: f32, y: f32, z: f32) -> Self {
-        Self { x, y, z }
+        Self {
+            x,
+            y,
+            z,
+        }
     }
 
     pub fn zero() -> Self {
         Self::new(0.0, 0.0, 0.0)
+    }
+
+    pub fn normalize(&self) -> Self {
+        let length = (self.x * self.x + self.y * self.y + self.z * self.z).sqrt();
+        Self::new(self.x / length, self.y / length, self.z / length)
+    }
+
+    pub fn dot(&self, rhs: Self) -> f32 {
+        self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+    }
+
+    pub fn cross(&self, rhs: Self) -> Self {
+        Self::new(self.y * rhs.z - self.z * rhs.y, self.z * rhs.x - self.x * rhs.z, self.x * rhs.y - self.y * rhs.x)
+    }
+}
+
+impl From<(f32, f32, f32)> for Vec3 {
+    fn from(item: (f32, f32, f32)) -> Self {
+        Self::new(item.0, item.1, item.2)
+    }
+}
+
+impl From<[f32; 3]> for Vec3 {
+    fn from(item: [f32; 3]) -> Self {
+        Self::new(item[0], item[1], item[2])
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Self;
+    fn neg(self) -> Self {
+        Self::new(-self.x, -self.y, -self.z)
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        Vec3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self {
+        Vec3::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
     }
 }
 
@@ -58,9 +116,13 @@ impl Mul<f32> for Vec4 {
 }
 
 impl Vec4 {
-    #[rustfmt::skip]
     pub fn new(x: f32, y: f32, z: f32, w: f32) -> Self {
-        Self { x, y, z, w }
+        Self {
+            x,
+            y,
+            z,
+            w,
+        }
     }
 
     pub fn zero() -> Self {
@@ -75,12 +137,16 @@ impl Vec4 {
             w: self.w.abs(),
         }
     }
+
+    pub fn dot(&self, rhs: Self) -> f32 {
+        self.x * rhs.x + self.y * rhs.y + self.z * rhs.z + self.w * rhs.w
+    }
 }
 
 impl Sub for Vec4 {
     type Output = Self;
 
-    fn sub(self, rhs: Self) -> Self::Output {
+    fn sub(self, rhs: Self) -> Self {
         Self {
             x: self.x - rhs.x,
             y: self.y - rhs.y,
@@ -150,7 +216,8 @@ impl Mat4 {
     }
 
     #[rustfmt::skip]
-    pub fn translate(x: f32, y: f32, z: f32) -> Self {
+    pub fn translate<T: Into<Vec3>>(t: T) -> Self {
+        let Vec3 {x, y, z} = t.into();
         Self([
             1.0, 0.0, 0.0,   x,
             0.0, 1.0, 0.0,   y,
@@ -185,8 +252,15 @@ impl Mat4 {
 
     #[rustfmt::skip]
     #[allow(unused_variables)]
-    pub fn look_at(eye: (f32, f32, f32), at: (f32, f32, f32), up: (f32, f32, f32)) -> Self {
-        //todo!();
+    pub fn look_at<T: Into<Vec3>>(eye: T, at: T, up: T) -> Self {
+        let forward = (at.into() - eye.into()).normalize();
+        let up = up.into().normalize();
+        let right = forward.cross(up).normalize();
+
+        //let r = Mat4::rotate();
+        let t = Mat4::translate(-forward);
+
+        // TODO
         Self([
             1.0, 0.0, 0.0, 0.0,
             0.0, 1.0, 0.0, 0.0,
@@ -195,18 +269,24 @@ impl Mat4 {
         ])
     }
 
+    /// Produces a Matrix to convert the given a rectangular cuboid, into a cube from -1 to 1 in
+    /// all 3 axis
+    /// X: Left   < Right  <=>  -1, +1
+    /// Y: Bottom < Top    <=>  -1, +1
+    /// Z: Near   < Far    <=>  -1, +1
     #[rustfmt::skip]
     pub fn ortho(left: f32, right: f32, bottom: f32, top: f32, near: f32, far: f32) -> Self {
         Self([
-            2.0 / (right - left),                  0.0,                0.0, 0.0,
-                             0.0, 2.0 / (top - bottom),                0.0, 0.0,
-                             0.0,                  0.0, 2.0 / (far - near), 0.0,
-                            0.0,                   0.0,                0.0, 1.0,
+            2.0 / (right - left),                  0.0,                0.0, -(right + left) / (right - left),
+                             0.0, 2.0 / (top - bottom),                0.0, -(top + bottom) / (top - bottom),
+                             0.0,                  0.0, 2.0 / (far - near), -(far + near)   / (far -   near),
+                             0.0,                  0.0,                0.0,                              1.0,
         ])
     }
 
     #[rustfmt::skip]
     pub fn perspective(fovy_radians: f32, aspect: f32, near: f32, far: f32) -> Self {
+        // TODO
         let focal_length = 1.0 / (fovy_radians / 2.0).tan();
         Self([
             focal_length / aspect,           0.0,                       0.0,                       0.0,
@@ -250,6 +330,22 @@ impl Mul for Mat4 {
 mod tests {
     use super::*;
 
+    macro_rules! assert_eq_f32 {
+        ($a:expr, $b:expr) => {
+            let diff = ($a - $b).abs();
+            assert!(diff <= f32::EPSILON, "diff: {}, EPSILON: {}", diff, f32::EPSILON);
+        };
+    }
+
+    macro_rules! assert_eq_v4 {
+        ($a:expr, $b:expr) => {
+            assert_eq_f32!($a.x, $b.x);
+            assert_eq_f32!($a.y, $b.y);
+            assert_eq_f32!($a.z, $b.z);
+            assert_eq_f32!($a.w, $b.w);
+        };
+    }
+
     #[test]
     #[rustfmt::skip]
     fn multiplication() {
@@ -269,7 +365,7 @@ mod tests {
     #[test]
     fn translation() {
         let v = Vec4::new(10.0, 10.0, 10.0, 1.0);
-        let m = Mat4::translate(10.0, 0.0, 0.0);
+        let m = Mat4::translate((10.0, 0.0, 0.0));
         assert_eq!(m * v, Vec4::new(20.0, 10.0, 10.0, 1.0));
     }
 
@@ -297,5 +393,81 @@ mod tests {
     }
 
     #[test]
+    fn ortho() {
+        // X
+        let left = 3.0;
+        let right = 23.0;
+
+        // Y
+        let bottom = -2.0;
+        let top = 10.0;
+
+        // Z
+        let near = 1.0;
+        let far = 100.0;
+
+        let m = Mat4::ortho(left, right, bottom, top, near, far);
+
+        let left_bottom_near = m * Vec4::new(left, bottom, near, 1.0);
+        let right_bottom_near = m * Vec4::new(right, bottom, near, 1.0);
+        let left_top_near = m * Vec4::new(left, top, near, 1.0);
+        let right_top_near = m * Vec4::new(right, top, near, 1.0);
+
+        let left_bottom_far = m * Vec4::new(left, bottom, far, 1.0);
+        let right_bottom_far = m * Vec4::new(right, bottom, far, 1.0);
+        let left_top_far = m * Vec4::new(left, top, far, 1.0);
+        let right_top_far = m * Vec4::new(right, top, far, 1.0);
+
+        assert_eq_v4!(left_bottom_near, Vec4::new(-1.0, -1.0, -1.0, 1.0));
+        assert_eq_v4!(right_bottom_near, Vec4::new(1.0, -1.0, -1.0, 1.0));
+        assert_eq_v4!(left_top_near, Vec4::new(-1.0, 1.0, -1.0, 1.0));
+        assert_eq_v4!(right_top_near, Vec4::new(1.0, 1.0, -1.0, 1.0));
+
+        assert_eq_v4!(left_bottom_far, Vec4::new(-1.0, -1.0, 1.0, 1.0));
+        assert_eq_v4!(right_bottom_far, Vec4::new(1.0, -1.0, 1.0, 1.0));
+        assert_eq_v4!(left_top_far, Vec4::new(-1.0, 1.0, 1.0, 1.0));
+        assert_eq_v4!(right_top_far, Vec4::new(1.0, 1.0, 1.0, 1.0));
+    }
+
+    #[test]
     fn perspective() {}
+
+    #[test]
+    fn ortho_translate_scale() {
+        // X
+        let left = 3.0;
+        let right = 23.0;
+
+        // Y
+        let bottom = -2.0;
+        let top = 10.0;
+
+        // Z
+        let near = 1.0;
+        let far = 100.0;
+
+        let m = Mat4::scale(1.0, 1.0, 0.5)
+            * Mat4::translate((0.0, 0.0, 1.0))
+            * Mat4::ortho(left, right, bottom, top, near, far);
+
+        let left_bottom_near = m * Vec4::new(left, bottom, near, 1.0);
+        let right_bottom_near = m * Vec4::new(right, bottom, near, 1.0);
+        let left_top_near = m * Vec4::new(left, top, near, 1.0);
+        let right_top_near = m * Vec4::new(right, top, near, 1.0);
+
+        let left_bottom_far = m * Vec4::new(left, bottom, far, 1.0);
+        let right_bottom_far = m * Vec4::new(right, bottom, far, 1.0);
+        let left_top_far = m * Vec4::new(left, top, far, 1.0);
+        let right_top_far = m * Vec4::new(right, top, far, 1.0);
+
+        assert_eq_v4!(left_bottom_near, Vec4::new(-1.0, -1.0, 0.0, 1.0));
+        assert_eq_v4!(right_bottom_near, Vec4::new(1.0, -1.0, 0.0, 1.0));
+        assert_eq_v4!(left_top_near, Vec4::new(-1.0, 1.0, 0.0, 1.0));
+        assert_eq_v4!(right_top_near, Vec4::new(1.0, 1.0, 0.0, 1.0));
+
+        assert_eq_v4!(left_bottom_far, Vec4::new(-1.0, -1.0, 1.0, 1.0));
+        assert_eq_v4!(right_bottom_far, Vec4::new(1.0, -1.0, 1.0, 1.0));
+        assert_eq_v4!(left_top_far, Vec4::new(-1.0, 1.0, 1.0, 1.0));
+        assert_eq_v4!(right_top_far, Vec4::new(1.0, 1.0, 1.0, 1.0));
+    }
 }
