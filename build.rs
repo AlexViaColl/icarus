9126b76dddb050cbf7e7a7b96c769ec12518e281
AlexViaColl <alexviacoll@gmail.com>
2022-04-20 20:03:46 +0200

    Get rid of ./

--- build.rs
+++ build.rs
@@ -31,7 +31,7 @@ fn main() {
     assert!(output.status.success(), "stderr: {}\nstdout: {}", stderr, stdout);
 
     // Download flappy bird assets
-    let path = "./assets/textures/flappy";
+    let path = "assets/textures/flappy";
     let url = "https://raw.githubusercontent.com/samuelcust/flappy-bird-assets/master/sprites";
     //let url = "https://github.com/samuelcust/flappy-bird-assets/raw/master/sprites";
     download_if_not_present(format!("{}/background-day.png", path), format!("{}/background-day.png", url));
