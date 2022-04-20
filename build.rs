2d4d2a8cd9798d33df17a8bf71d4cedcecaebf08
AlexViaColl <alexviacoll@gmail.com>
2022-04-20 18:48:13 +0200

    Update build.rs to download flappy bird assets

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
+use std::ffi::OsStr;
 use std::os::unix::fs::PermissionsExt;
 use std::path::Path;
 use std::process::Command;
@@ -9,12 +10,18 @@ fn main() {
     println!("cargo:rerun-if-changed=compile.sh");
     println!("cargo:rerun-if-changed=assets/shaders/");
 
-    if !Path::new("stb_image.h").exists() {
-        Command::new("/usr/bin/wget")
-            .arg("https://raw.githubusercontent.com/nothings/stb/master/stb_image.h")
-            .status()
-            .unwrap();
-    }
+    // Download flappy bird assets
+    let path = "assets/textures/flappy";
+    let url = "https://raw.githubusercontent.com/samuelcust/flappy-bird-assets/sprites";
+    download_if_not_present(format!("{}/background-day.png", path), format!("{}/background-day.png", url));
+    download_if_not_present(format!("{}/base.png", path), format!("{}/base.png", url));
+    download_if_not_present(format!("{}/bluebird-downflap.png", path), format!("{}/bluebird-downflap.png", url));
+    download_if_not_present(format!("{}/bluebird-midflap.png", path), format!("{}/bluebird-midflap.png", url));
+    download_if_not_present(format!("{}/bluebird-upflap.png", path), format!("{}/bluebird-upflap.png", url));
+    download_if_not_present(format!("{}/pipe-green.png", path), format!("{}/pipe-green.png", url));
+
+    // Download stb_image
+    download_if_not_present("stb_image.h", "https://raw.githubusercontent.com/nothings/stb/master/stb_image.h");
 
     if !Path::new("glslc").exists() {
         Command::new("/usr/bin/wget")
@@ -30,4 +37,10 @@ fn main() {
     let stdout = String::from_utf8(output.stdout).unwrap();
     let stderr = String::from_utf8(output.stderr).unwrap();
     assert!(output.status.success(), "stderr: {}\nstdout: {}", stderr, stdout);
+}
+
+fn download_if_not_present<P: AsRef<OsStr>>(path: P, url: P) {
+    if !Path::new(&path).exists() {
+        Command::new("/usr/bin/wget").arg("-O").arg(path).arg(url).status().unwrap();
+    }
 }
