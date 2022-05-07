4fd96e9d07469bd2ae8fe5953e4866c28583db9d
AlexViaColl <alexviacoll@gmail.com>
2022-05-07 16:45:24 +0200

    Move bitflag_struct and bitflag_enum to macros module

--- src/vk.rs
+++ src/vk.rs
@@ -3,63 +3,7 @@
 
 use std::ffi::c_void;
 
-use crate::{cstr, opaque};
-
-macro_rules! bitflag_struct {
-    ($struct_name:ident : $enum_name:ident) => {
-        #[derive(Copy, Clone, Default)]
-        #[repr(C)]
-        pub struct $struct_name {
-            pub value: u32,
-        }
-        impl From<u32> for $struct_name {
-            fn from(value: u32) -> Self {
-                Self {
-                    value,
-                }
-            }
-        }
-        impl std::fmt::Debug for $struct_name {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                let mut value = self.value;
-                for i in 0..31 {
-                    let flag = (value & 1) << i;
-                    value >>= 1;
-                    if flag != 0 {
-                        write!(f, "{:?}({})", $enum_name::from(flag), flag)?;
-                        if value > 0 {
-                            write!(f, " | ")?;
-                        }
-                    }
-                }
-                write!(f, "")
-            }
-        }
-    };
-}
-
-macro_rules! bitflag_enum {
-    (
-        $enum_name:ident {
-            $($variant:ident = $value:expr,)*
-        }
-    ) => {
-        #[repr(u32)]
-        #[derive(Debug)]
-        pub enum $enum_name {
-            $($variant = $value,)*
-        }
-        impl From<u32> for $enum_name {
-            fn from(flag: u32) -> Self {
-                match flag {
-                    $($value => $enum_name::$variant,)*
-                    n => panic!("Invalid flag: {}", n),
-                }
-            }
-        }
-        $(pub const $variant: u32 = $value;)*
-    };
-}
+use crate::{bitflag_enum, bitflag_struct, cstr, opaque};
 
 #[link(name = "vulkan")]
 extern "C" {
